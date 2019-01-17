766546ce55d3a833af7d4b43738ff6cd39dadd73
Takagishi Kenshin <h.r.y.kt629@gmail.com>
2019-01-18 00:26:19 +0900

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ああああああああああああああ
-          </a>
-        </header>
       </div>
     );
   }
